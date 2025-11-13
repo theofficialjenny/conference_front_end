3a0125ff0f36ba41c2e16df22c068b0906b0d7fa
theofficialjenny <jenniferbenjamin235@gmail.com>
2025-11-13 14:42:46 +1300

    Important changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/';
+axios.defaults.withCredentials = true;
 
 export const getRooms = () => axios.get(`${API_BASE_URL}rooms/`);
 export const getReservations = () => axios.get(`${API_BASE_URL}reservations/`);
